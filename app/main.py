2387eac6ce1c810f46ce5d7ad2d0b0d773894dc1
Dunstan Dsouza <dunstan998@gmail.com>
2024-02-22 05:44:46 -0500

    Respond to a PING request

--- app/main.py
+++ app/main.py
@@ -5,11 +5,14 @@ import socket
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
+    pong = "+PONG\r\n"
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
 
+    with conn:
+        conn.recv(1024)
+        conn.send(pong.encode())
 
 if __name__ == "__main__":
     main()
