8a1613f91148caf52d0c03e9e8213fc7952af79a
Dunstan Dsouza <dunstan998@gmail.com>
2024-02-27 05:04:29 -0500

    Handle concurrent clients sending multiple PINGs  on different connections

--- app/main.py
+++ app/main.py
@@ -10,10 +10,13 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
 
-    with conn:
-        while True:
-            conn.recv(1024)
-            conn.send(pong.encode())
+    
+    while True:
+        conn, addr = server_socket.accept() # wait for client
+        with conn:
+            while True:
+                conn.recv(1024)
+                conn.send(pong.encode())
 
 if __name__ == "__main__":
     main()
