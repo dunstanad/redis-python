292787a09211629eb9751e65c818a811f1588b25
Dunstan Dsouza <dunstan998@gmail.com>
2024-02-27 23:16:12 -0500

    Implementing ECHO command

--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
-# Uncomment this to pass the first stage
 import socket
 import threading
 
 
-def handleConnections(conn, pong):
+# def handleConnections(conn, pong):
+#     with conn:
+#         while True:
+#             data = conn.recv(1024)
+#             if not data:
+#                 break
+#             conn.send(pong.encode())
+
+
+def handleConnections(conn):
     with conn:
         while True:
-            data = conn.recv(1024)
+            data = conn.recv(1024).decode()
+            print(data)
             if not data:
                 break
-            conn.send(pong.encode())
-
+            #conn.send(pong.encode())
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -18,12 +26,11 @@ def main():
     pong = "+PONG\r\n"
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-   # conn, addr = server_socket.accept() # wait for client
 
     
     while True:
         conn, addr = server_socket.accept() # wait for client
-        threading.Thread(target=handleConnections, args=(conn,pong)).start()
+        threading.Thread(target=handleConnections, args=conn).start()
 
 if __name__ == "__main__":
     main()
