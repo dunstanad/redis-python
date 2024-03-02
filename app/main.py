da4b41b1d96af88dc04e9b80038e5003c36607ed
Dunstan Dsouza <dunstan998@gmail.com>
2024-03-02 04:27:04 -0500

    Implementing INFO command on replica

--- app/main.py
+++ app/main.py
@@ -158,6 +158,7 @@ def main():
     #server_socket = socket.create_server(("localhost", portNumber), reuse_port=True) 
 
     while True:
+         
         server_socket = socket.create_server(("localhost", portNumber), reuse_port=True)
         conn, addr = server_socket.accept() # wait for client
         threading.Thread(target=handleConnections, args=(conn, isMaster)).start()
